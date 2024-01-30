3931ed54ef1e8a831da9ddb7f0efbc9a405ef20d
Leiv Fredrik Berge <leiv.fredrik.berge@mattilsynet.no>
2024-01-30 10:54:24 +0100

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,100 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect, useRef, useState } from "react";
+import rough from "roughjs";
+import backend from "./backend.json";
+import frontend from "./frontend.json";
+import infrastructure from "./infrastructure.json";
 
 export default function Home() {
+  const ref = useRef(null);
+  const [techs, setTechs] = useState(getTech("all"));
+
+  useEffect(() => {
+    if (ref.current === null) return;
+    const rc = rough.canvas(ref.current, { options: { roughness: 0.2 } });
+    const canvas = ref.current as HTMLCanvasElement;
+    const ctx = canvas.getContext("2d")!;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.font = "12px sans-serif ";
+    ctx.fillStyle = "white";
+
+    rc.circle(600, 500, 300, {
+      stroke: "rgba(9, 121, 105,0.4)",
+    });
+    rc.circle(600, 500, 600, {
+      stroke: "rgba(8, 143, 143, 0.4)",
+    });
+    rc.circle(600, 500, 800, {
+      stroke: "rgba(255, 191, 0.4)",
+    });
+    rc.circle(600, 500, 1000, {
+      stroke: "rgba(250,128,114,0.4)",
+    });
+
+    techs.forEach((tech) => {
+      const x = 600 - tech.x;
+      const y = 500 - tech.y;
+      rc.circle(x, y, 20, {
+        fill: getColor(tech.state),
+        fillWeight: 3,
+      });
+      ctx.fillText(tech.name, x + 10, y + 10);
+    });
+  }, [techs]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <>
+      <canvas ref={ref} height="1000" width="1200" className="m-auto" />
+      <select
+        className="m-4 text-white bg-gray-900 p-2"
+        onChange={(e) => setTechs(getTech(e.target.value))}
+      >
+        <option value="frontend">Frontend</option>
+        <option value="backend">Backend</option>
+        <option value="infrastructure">Infra</option>
+        <option value="all">All</option>
+      </select>
+      <ul className="p-4">
+        {techs.map((tech) => (
+          <li key={tech.name} className="flex flex-col pb-2 max-w-prose">
+            <span>{tech.name}</span>
+            <span>{tech.description}</span>
+            <span>{tech.state}</span>
+          </li>
+        ))}
+      </ul>
+    </>
   );
+}
+
+function getColor(state: string) {
+  switch (state) {
+    case "adopt":
+      return "rgba(9, 121, 105,0.4)";
+    case "trial":
+      return "rgba(8, 143, 143, 0.4)";
+    case "assess":
+      return "rgba(255, 191, 0.4)";
+    case "hold":
+      return "rgba(250,128,114,0.4)";
+    case "avoid":
+      return "rgba(255, 0, 0,0.4)";
+    case "none":
+      return "rgba(0, 0, 0,0.4)";
+  }
+}
+
+function getTech(selected: string) {
+  switch (selected) {
+    case "frontend":
+      return frontend;
+    case "backend":
+      return backend;
+    case "infrastructure":
+      return infrastructure;
+    case "all":
+      return [...frontend, ...backend, ...infrastructure];
+  }
+  return frontend;
 }
